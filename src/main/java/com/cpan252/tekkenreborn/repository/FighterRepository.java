743d59283dd042cf85f8ae6fd52d1b8dcc0d7986
Jason <jason.tieu@Hotmail.com>
2023-02-07 16:32:53 -0500

    Changed class To interface (Lab3) Completed

--- src/main/java/com/cpan252/tekkenreborn/repository/FighterRepository.java
+++ src/main/java/com/cpan252/tekkenreborn/repository/FighterRepository.java
@@ -7,7 +7,7 @@ import com.cpan252.tekkenreborn.model.Fighter;
 
 //It will be an interface that defines operations with the fighter
 //table in the database
-public class FighterRepository {
+public interface FighterRepository {
     Iterable<Fighter> findAll();
 
     Optional<Fighter> findById(Long id);
